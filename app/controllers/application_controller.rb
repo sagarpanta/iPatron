e9b246db254af1e1da556aa7ade915542f294353
Sagar Pant <sagar_007p@yahoo.com>
2012-09-09 09:54:50 -0500

    xyz

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
-  
- include Mobylette::RespondToMobileRequests
-   
-	mobylette_config do |config|
-		config[:fall_back] = :html
-		config[:skip_xhr_requests] = false
-	end
+
 end
